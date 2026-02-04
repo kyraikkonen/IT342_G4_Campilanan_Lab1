d6422ed80550a89cb31e052d1c5cbfaa9b84936c
LAB13_USER ON WS30 <L13X16W30@LabHCI.net>
2026-02-04 17:01:29 +0800

    Add Spring Boot project files

---
+++ backend/miniauth/src/main/java/com/miniauth/miniauth/MiniauthApplication.java
@@ -0,0 +1,13 @@
+package com.miniauth.miniauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniauthApplication.class, args);
+	}
+
+}
---
+++ backend/miniauth/src/test/java/com/miniauth/miniauth/MiniauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniauth.miniauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
